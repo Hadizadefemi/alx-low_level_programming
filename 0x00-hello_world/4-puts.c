5b2dc7aa0bd34180528ae944ee5a015e7b0c1c13
Hadizadefemi <adefemihadizi2020@gmail.com>
2022-09-08 15:35:06 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,11 +4,11 @@
  * main - print the string in the main function
  *
  *Description: using the main function
- * Return 0
+ * Return: Always 0
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 
 }
