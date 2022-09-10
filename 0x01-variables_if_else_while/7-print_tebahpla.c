6afc75820abc08c0c74d5d1593994f36f9d2af8c
Hadizadefemi <111155656+Hadizadefemi@users.noreply.github.com>
2022-09-10 01:27:19 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,7 +3,7 @@
 
 /**
  * main - main block
- * Description: prints the lowercase alphabet in reverse, followed by a new line.
+ * Description: prints the lowercase alphabet in reverse.
  * Return: 0
  */
 int main(void)
