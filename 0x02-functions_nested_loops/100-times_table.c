3ee451d8527c8e12c17d3bf0eebb01929d72912d
Hadizadefemi <adefemihadizi2020@gmail.com>
2022-09-14 07:48:59 -0700

    half done

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints times table
+ * @n : times table to use 
+ * Return : void
+ */
+void print_times_table(int n)
+{
+	int a = 0, rep, b;
+
+	if (n < 0 || n > 15)
+		return;
+
+	while (a <= n)
+}
