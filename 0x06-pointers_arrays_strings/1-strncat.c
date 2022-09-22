a09604abb8f3d2db65b3d8e139dbd6703e5fc3f2
Hadizadefemi <111155656+Hadizadefemi@users.noreply.github.com>
2022-09-22 11:10:54 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,21 +1,20 @@
 #include "main.h"
-
 /**
  * _strncat - concatenates two string,
  * @dest: destination.
  * @src: source.
  * @n: amount of bytes used from src.
- * Return: the pointer to dest
+ * Return: the pointer to dust.
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
 	int count = 0, count2 = 0;
-	
+
 	while (*(dest + count) != '\0')
 	{
 		count++;
 	}
+
 	while (count2 < n)
 	{
 		*(dest + count) = *(src + count2);
