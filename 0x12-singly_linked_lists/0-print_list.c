944d1272f8ed65802963186ae3b606d38a8417a6
Hadizadefemi <adefemihadizi2020@gmail.com>
2022-10-21 13:09:31 -0700

    Task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: singly linked list
+ *
+ * Return: the number of nodes
+ */
+
+
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This folder contains files of C programs for practicing linkedlist
